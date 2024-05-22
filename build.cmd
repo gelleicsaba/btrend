9b7cebca4e4af95b4c31f4107513a26bfc08e366
gelleicsaba <gellei.csaba@gmail.com>
2024-05-22 12:57:22 +0200

    Fix

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-python.exe btrend.py "-in=dummy.txt" "-out=out.txt" -step=5 -s -t
-python.exe btrend.py "-in=dummy.txt" "-out=out-plus-comments.txt" -step=5 -t
+python.exe btrend.py "-in=dummy.txt" "-out=out.txt" -step=5 -s -t -p
+python.exe btrend.py "-in=dummy.txt" "-out=out-plus-comments.txt" -step=5 -t -p
 c64list out.txt -ovr -prg:out.prg
